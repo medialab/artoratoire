30962d56c32d67526cfaf930cf6ac3badee87538
mydu <dumengying1989@gmail.com>
2017-10-16 11:37:41 +0200

    change to class variables

--- src/components/StreamingWave/MicRecorder.js
+++ src/components/StreamingWave/MicRecorder.js
@@ -1,38 +1,33 @@
 import AudioContext from './AudioContext';
 
-let analyser;
-let audioCtx;
-let mediaRecorder;
-let chunks = [];
-let startTime;
-let mediaOptions;
-let onStartCallback;
-let onStopCallback;
-
 const constraints = {audio: true, video: false}; // constraints - only audio needed
 
 export class MicRecorder {
   constructor(onStart, onStop, options) {
-    onStartCallback = onStart;
-    onStopCallback = onStop;
-    mediaOptions = options;
+    this.onStartCallback = onStart;
+    this.onStopCallback = onStop;
+    this.options = options;
+
+    this.mediaRecorder = null;
+    this.chunks = [];
+    this.startTime = null;
   }
 
   setupRecorder() {
     if (navigator.mediaDevices) {
       console.log('getUserMedia supported.');
       navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-        audioCtx = AudioContext.getAudioContext();
-        analyser = AudioContext.getAnalyser();
+        const audioCtx = AudioContext.getAudioContext();
+        const analyser = AudioContext.getAnalyser();
 
-        mediaRecorder = new window.MediaRecorder(stream);
+        this.mediaRecorder = new window.MediaRecorder(stream);
 
         const source = audioCtx.createMediaStreamSource(stream);
         source.connect(analyser);
 
-        mediaRecorder.onstop = this.onStop;
-        mediaRecorder.ondataavailable = (event) => {
-          chunks.push(event.data);
+        this.mediaRecorder.onstop = () => this.onStop();
+        this.mediaRecorder.ondataavailable = (event) => {
+          this.chunks.push(event.data);
         };
       });
     }
@@ -42,30 +37,29 @@ export class MicRecorder {
   }
 
   startRecording() {
-    startTime = Date.now();
-    mediaRecorder.start();
-    if (onStartCallback) onStartCallback();
+    this.startTime = Date.now();
+    this.mediaRecorder.start();
+    if (this.onStartCallback) this.onStartCallback();
   }
 
   stopRecording() {
-    if (mediaRecorder && mediaRecorder.state !== 'inactive') {
-      mediaRecorder.stop();
+    if (this.mediaRecorder && this.mediaRecorder.state !== 'inactive') {
+      this.mediaRecorder.stop();
       // audioCtx.suspend();
     }
   }
 
   onStop() {
-    const blob = new Blob(chunks, {type: mediaOptions.mimeType});
-    chunks = [];
-
+    const blob = new Blob(this.chunks, {type: this.options.mimeType});
+    this.chunks = [];
     const blobObject = {
       blob,
-      startTime,
+      startTime: this.startTime,
       stopTime: Date.now(),
-      options: mediaOptions,
+      options: this.options,
       blobURL: window.URL.createObjectURL(blob)
     };
 
-    if (onStopCallback) onStopCallback(blobObject);
+    if (this.onStopCallback) this.onStopCallback(blobObject);
   }
 }
