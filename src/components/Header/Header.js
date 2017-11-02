ea201c5831729c0ab31a1071c4a2324538fe0123
mydu <dumengying1989@gmail.com>
2017-11-02 12:23:49 +0100

    update intro text and speech_list

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,40 +1,44 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import './Header.scss';
+import Scrollchor from 'react-scrollchor';
 
+import './Header.scss';
 import LanguageSwitch from '../LanguageSwitch/LanguageSwitch';
 
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      scrollTop: 0
-    };
-    // this.handleScroll = this.handleScroll.bind(this);
   }
-
-  // componentDidMount() {
-  //   window.addEventListener('scroll', this.handleScroll);
-  // }
-  // componentUnmount() {
-  //   window.removeEventListener('scroll', this.handleScroll);
-  // }
-
-  // handleScroll() {
-  //   this.setState({
-  //     scrollTop: window.scrollY
-  //   });
-  // }
 
   render() {
     const translate = this.context.t;
     return (
-      <div className="aort-Header">
-        <div className="columns is-centered">
-          <div className="column is-half has-text-centered">
-            <p className="title is-2">Artoratoire</p>
-            <p className="subtitle is-4">{translate('subtitle')}</p>
-            <LanguageSwitch lang={this.props.lang} setLanguage={this.props.setLanguage} />
+      <div className="aort-Header container">
+        <div className="columns is-centered content">
+          <div className="column is-half">
+            <div className="has-text-centered">
+              <p className="title is-2">Art oratoire</p>
+              <p className="subtitle is-4">{translate('subtitle')}</p>
+              <LanguageSwitch className={'is-white'} lang={this.props.lang} setLanguage={this.props.setLanguage} />
+            </div>
+            <div>
+              <p>{translate('intro-first')}</p>
+              <p>{translate('intro-second')}</p>
+              <ul>
+                <li>{translate('intro-step-one')}</li>
+                <li>{translate('intro-step-two')}</li>
+                <li>{translate('intro-step-three')}</li>
+              </ul>
+            </div>
+            <div className="link-down has-text-centered">
+              <Scrollchor to="#main">
+                <span>Start Playing</span>
+                <br />
+                <span className="icon">
+                  <i className="fa fa-long-arrow-down"></i>
+                </span>
+              </Scrollchor>
+            </div>
           </div>
         </div>
       </div>
