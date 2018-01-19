74ca4c566c403276caa81fb46bbcc6a52612ed78
Paul Girard <paul.girard@sciencespo.fr>
2018-01-19 14:58:05 +0100

    dynamic sampleRate assignement

--- src/constants/AudioConstants.js
+++ src/constants/AudioConstants.js
@@ -1,5 +1,6 @@
 export const SAMPLE_RATE = 4096;
 export const THRESHOLD = 0.01;
-export const SEC_BUFFER = (navigator.userAgent.indexOf('Chrome') !== -1) ? 44100 : 48000;
-export const MIN_DURATION = 0.3;
 
+const  AudioContext = window.AudioContext || window.webkitAudioContext
+export const SEC_BUFFER = new AudioContext().sampleRate;
+export const MIN_DURATION = 0.3;
